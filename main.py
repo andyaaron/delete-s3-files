934a068e8c2f01e50758dc47dab2c5d5c8cef6c0
andyaaron <aaron.richard.andy@gmail.com>
2023-06-26 11:31:06 -0400

    move some logic check matched objs before setting private files change return to continue

--- main.py
+++ main.py
@@ -11,10 +11,9 @@ def check_object_keys(s3_object_key, json_objects_file, matched_objs):
     with open(json_objects_file, 'r') as file:
         json_data = json.load(file)
 
-    if s3_object_key.endswith('jpg') or s3_object_key.endswith('png'):
-        for obj in json_data:
-            if obj['guid'] in s3_object_key:
-                matched_objs.append(s3_object_key)
+    # if any of the objects in `json_data` have a `guid` value that is in `s3_object_key`
+    if any(obj['guid'] in s3_object_key for obj in json_data):
+        matched_objs.append(s3_object_key)
 
     return matched_objs
 
@@ -36,14 +35,8 @@ def set_s3_objects_acl_private(bucket_name, object_keys):
         for grant in grants:
             grantee = grant.get('Grantee', {})
             permission = grant.get('Permission')
-
-            # If the grantee is not EVERYONE, skip the grant.
-            if grantee.get('URI') != 'http://acs.amazonaws.com/groups/global/AllUsers':
-                continue
-
             # Check if the grantee is 'Everyone' and the permission is 'READ'
             if (
-                isinstance(grantee, dict) and
                 grantee.get('URI') == 'http://acs.amazonaws.com/groups/global/AllUsers' and
                 permission == 'READ'
             ):
@@ -55,8 +48,8 @@ def set_s3_objects_acl_private(bucket_name, object_keys):
                 )
                 count += 1
 
-        # We're done looping through the s3 object grants. return the count
-        return count
+    # We're done looping through the s3 object grants. return the count
+    return count
 
 
 def compare_s3_objects_with_json(bucket_name, json_objects, output_file):
@@ -68,7 +61,7 @@ def compare_s3_objects_with_json(bucket_name, json_objects, output_file):
     # Setup our paginator
     paginator = s3.get_paginator('list_objects_v2')
 
-    for year in range(2010, 2015):
+    for year in range(2010, 2016):
         prefix = "test_delete/" + str(year)
         page_iterator = paginator.paginate(Bucket=bucket_name, Prefix=prefix)
         count = 0
@@ -76,16 +69,22 @@ def compare_s3_objects_with_json(bucket_name, json_objects, output_file):
         # Paginate through our results
         for page in page_iterator:
             if 'Contents' not in page:
-                return
+                continue
 
             # Iterate over the S3 objects and compare with JSON objects
             for obj in page['Contents']:
+                s3_object_key = obj['Key']
                 # Check if the S3 object key is in our json file
-                matched_objs = check_object_keys(obj['Key'], json_objects, matched_objs)
-                count += 1
+                # We also check for any additional files with the same name. a lot of files have different sizes.
+                if s3_object_key.endswith(('jpg', 'png')):
+                    # append new object keys. @TODO: Should we append here instead of in check_object_keys?
+                    matched_objs = check_object_keys(s3_object_key, json_objects, matched_objs)
+                    count += 1
 
             # change ACL to private
-            private_files = set_s3_objects_acl_private(bucket_name, matched_objs)
+            if len(matched_objs) > 0:
+                private_files = set_s3_objects_acl_private(bucket_name, matched_objs)
+
             # at the end, write our privatized object keys to a new file
             print("year: ", year)
             print("file count: ", count)
